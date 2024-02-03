8e40bad6eb4cc2b90285381c553fb84df342d5ca
FaithOgendi <faithbrianna01@gmail.com>
2024-02-03 10:20:01 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,52 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskItems, setTaskItems] = useState(TASKS);
+  const [currectSelectedButtonIndex, setCurrectSelectedButtonIndex] =
+    useState(0);
+
+  const changeSelectedButton = (index) => {// to change selected category button index
+    setCurrectSelectedButtonIndex(index);
+  };
+
+  const deleteTask = (taskIndex) => {// to delete a new task
+    const newTasks = taskItems.filter((_, index) => {//filtering tasks
+      if (index !== taskIndex) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    setTaskItems(newTasks);//updating with new list of tasks
+  };
+
+  const addTask = (task) => {// to add a new task
+    const newTasks = [...taskItems, task];
+    setTaskItems(newTasks);//updating with new list of tasks
+    console.log(newTasks);
+  };
+
+  const filtedTasks = taskItems.filter((task) => {// filtering tasks based on selected category
+    if (CATEGORIES[currectSelectedButtonIndex] === "All") {
+      return true;
+    } else {
+      return task.category === CATEGORIES[currectSelectedButtonIndex];
+    }
+  });
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        categoryIndex={currectSelectedButtonIndex}
+        onSelectedButtonChange={changeSelectedButton}
+      />
+      <NewTaskForm
+      categories={CATEGORIES.slice(1)}
+      onTaskFormSubmit={addTask}
+      />
+      <TaskList tasks={filtedTasks} onTaskDelete={deleteTask} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import { useState } from "react";// importing use state
+import React from "react"
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+
+  const [formData, setFormData] = useState({ text: "", category: "" });// state variables for managing form data
+  const optionsList = categories.map((category) => (//list of option elemnt for each category
+    <option key={category} value={category}>
+      {category}
+    </option>
+  ));
+
+  const handleOnChange = (e) => {
+    const newObj = { ...formData, [e.target.name]: e.target.value };
+    setFormData(newObj);
+  };
+
+  const handleFormSubmit = (e) => {// Handler function for form submission
+    e.preventDefault();
+    onTaskFormSubmit(formData);//Invoking the onTaskFormSubmit callback with the current form data
+  };
+
+  //rendering the new task component
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text"  value={formData.text}
+          onChange={handleOnChange}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category"    value={formData.category}
+          onChange={handleOnChange}>
           {/* render <option> elements for each category here */}
+          {optionsList}
         </select>
       </label>
       <input type="submit" value="Add task" />
